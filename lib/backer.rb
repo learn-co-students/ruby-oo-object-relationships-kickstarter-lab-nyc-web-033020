633120875f53203c55271c92ec641e3fcc2c79de
github username <github email address>
2020-04-03 14:46:56 -0400

    new methods backer

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,31 @@
 class Backer
   attr_reader :name
+
   def initialize (name)
       @name = name
   end
-  def backer_project(project)
-      ProjectBacker.project
+
+  def back_project(project)
+      ProjectBacker.new(project, self)
   end
+
+  def backed_projects
+    ProjectBacker.all.select { |projects| projects.backer == self }
+
+
+
+    # projects_arr = []
+    # ProjectBacker.all.each do |project|
+    #   if project.backer == self
+    #     projects_arr << ProjectBacker.all.project
+    #   end
+    #   return projects_arr
+    # end
+  end
+
+
 end
+
+
 # backer                project_backer              project
 #name                   backer - project            title
--- lib/project.rb
+++ lib/project.rb
@@ -1,6 +1,12 @@
 class Project
   attr_reader :title
+
   def initialize(title)
-      @title = title
+    @title = title
   end
+
+  def add_backer(backer)
+    ProjectBacker.new(self, backer)
+  end
+
 end
