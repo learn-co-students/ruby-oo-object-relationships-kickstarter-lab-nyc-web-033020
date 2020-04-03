ba95f527386b164e6523864988d3b09dd36257eb
Garen Sahagian <g.s.sahagian@gmail.com>
2020-04-03 14:39:26 -0400

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,1 +1,31 @@
-#Test comment for Garen and Taevon.
+class Backer
+    attr_reader :name
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+
+    def backed_projects
+        pb = ProjectBacker.all.select {|key| key.backer == self }
+        pb.map do |key|
+            key.project
+        end
+    end
+
+end
+
+
+num = [2,2,2,2,2]
+
+num.map do |key|
+    key+1
+end
+
+[]
+
+[2,2,2,2,2]
+
+[3,3,3,3,3]
