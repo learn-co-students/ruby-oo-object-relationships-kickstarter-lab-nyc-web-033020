010e40a10b38ecb5a9e4e2a291c8924e5c1d9df1
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-04-03 14:10:48 -0400

    updated kickstart

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,16 @@
 class Backer
+	@@all = [ ]
 		attr_reader :name
 		def initialize(name)
 				@name = name
+				Backer.all << self 
 		end
-end
+		def self.allge
+			@@all
+		end 
+		def back_project (project)
+			ProjectBacker.new(project,self)
+		end 
+
+	end
+
--- lib/project.rb
+++ lib/project.rb
@@ -1,6 +1,16 @@
 class Project
-		attr_reader :title
-		def initialize(name)
-				@title = name
-		end
+	attr_reader :title
+	@@all = [ ]
+	def initialize(name)
+			@title = name
+			Project.all << self 
+	end
+	def self.all
+		@@all
+	end 
+	def add_backer(backer)
+		ProjectBacker.new(self, backer)
+	end 
+
+
 end
