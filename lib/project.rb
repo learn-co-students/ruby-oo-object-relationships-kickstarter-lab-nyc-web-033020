6ed43c6f52e7d9189a43d3d3d037012efa2cac06
Edgar <edgar.a.rivera08@gmail.com>
2020-04-03 15:10:57 -0400

    Completed lab

--- lib/project.rb
+++ lib/project.rb
@@ -7,4 +7,12 @@ class Project
     def add_backer(backer)
         ProjectBacker.new(self, backer)
     end
+
+    def pb 
+        ProjectBacker.all.select {|pb| pb.project == self}
+    end
+
+    def backers
+       pp pb.map {|pb| pb.backer }
+    end
 end
