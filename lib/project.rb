a3361f17005b52f621dcdefe98591b4cc4ad8a4a
yuksalbek97 <yuksal97@gmail.com>
2020-04-03 14:20:56 -0400

    Finished pair programming

--- lib/project.rb
+++ lib/project.rb
@@ -4,5 +4,15 @@ class Project
   def initialize(title)
     @title = title
   end
+
+def add_backer(backer)
+ProjectBacker.new(self,backer)
+end
+
+def backers
+   ProjectBacker.all.select{|project_backer| project_backer.project == self}.map {|project_backer| project_backer.backer}
+
+end
+
 end
 
