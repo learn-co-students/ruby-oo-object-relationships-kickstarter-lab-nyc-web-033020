3852b57431797d159e0b2385fc8a758f7c3e050e
Garen Sahagian <g.s.sahagian@gmail.com>
2020-04-03 15:04:47 -0400

    Done.

--- lib/project_backer.rb
+++ lib/project_backer.rb
@@ -1,18 +1,4 @@
 class ProjectBacker
-<<<<<<< HEAD
-    attr_reader :project, :backer
-    @@all = []
-    def initialize(project, backer)
-        @project = project
-        @backer = backer
-        ProjectBacker.all << self
-    end
-
-    def self.all
-        @@all
-    end
-end
-=======
   @@all = []
 
   attr_reader :project, :backer
@@ -34,4 +20,3 @@ end
 
 
 end
->>>>>>> 7d152ddb94d91e24ebf43a318b400d477c27449c
