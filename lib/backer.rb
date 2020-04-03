e28b905181b2c525aedb0cee76570e9ebd9c26aa
dkersten <dkersten1994@gmail.com>
2020-04-03 14:26:40 -0400

    Wrote methods to access data from other classes

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,4 +8,16 @@ class Backer
   def back_project(project)
     ProjectBacker.new(project, self)
   end
+
+  def projects
+    ProjectBacker.all.select do |project|
+      project.backer == self
+    end
+  end
+
+  def backed_projects
+    projects.map do |project|
+      project.project
+    end
+  end
 end
--- lib/project.rb
+++ lib/project.rb
@@ -4,4 +4,20 @@ class Project
   def initialize(title)
     @title = title
   end
+
+  def add_backer(backer)
+    ProjectBacker.new(self, backer)
+  end
+
+  def associated_backers
+    ProjectBacker.all.select do |backer|
+      backer.project == self
+    end
+  end
+
+  def backers
+   associated_backers.map do |backer|
+    backer.backer
+   end
+  end
 end
