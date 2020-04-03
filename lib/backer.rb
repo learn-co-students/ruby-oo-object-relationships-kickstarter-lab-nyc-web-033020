ed28935121f20b2a75b8dcbb2c14da64bbdb2f00
Edgar <edgar.a.rivera08@gmail.com>
2020-04-03 14:18:32 -0400

    New instance of ProjectBacker

--- lib/backer.rb
+++ lib/backer.rb
@@ -4,5 +4,27 @@ class Backer
     def initialize(name)
         @name = name
     end
+
+    def back_project(project)
+        ProjectBacker.new(project, self)
+    end
+
+
+
+
+
+
+
+    # def pb
+    #     ProjectBacker.all.select do |project| 
+    #         project.backer == self
+    #     end
+    # end
+
+    # def back_project(project)
+    #     pb.map do |projects|
+    #         projects.backer == self
+    #     end
+    # end
     
 end
